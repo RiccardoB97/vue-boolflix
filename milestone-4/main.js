0c8c837c1e861eed4474a17864e53a4c25949f23
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-06-09 11:22:30 +0200

    Add header and body background, image shown as cards

---
+++ milestone-4/main.js
@@ -0,0 +1,42 @@
+const app = new Vue({
+    el: '#app',
+
+    data: {
+        query: '',
+        apiKey: "api_key=5170c70f94bcc05071b2466c1ed085ea",
+        movieUrl: "https://api.themoviedb.org/3/search/movie?",
+        films: [],
+        seriesUrl: 'https://api.themoviedb.org/3/search/tv?',
+        series: [],
+        flagUrl: "https://www.countryflags.io/",
+        imageUrl: "https://image.tmdb.org/t/p/w342"
+    },
+
+    methods: {
+        searchFilm() {
+            axios
+                .get(this.movieUrl + this.apiKey + '&query=' + this.query,
+            )
+                .then(resp => {
+                    this.films = resp.data.results;
+                })
+                .catch(e => {
+                    console.error(e);
+                })
+        },
+        searchSeries() {
+            axios
+                .get(this.seriesUrl + this.apiKey + '&query=' + this.query)
+                .then(resp => {
+                    this.series = resp.data.results
+                })
+                .catch(e => {
+                    console.error(e)
+                })
+        }
+    },
+    mounted() {
+
+    }
+
+})
